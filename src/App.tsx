12b0b1971aeee2c8a7bd179973d78cfbd792e359
SnowkzzZ <vihanncosmeticos@gmail.com>
2026-06-26 14:30:03 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Admin from './pages/Admin';
 import Intelligence from './pages/Intelligence';
 import MateriaisApoio from './pages/MateriaisApoio';
 import TreinamentosAoVivo from './pages/TreinamentosAoVivo';
-import TreinamentoDetalhe from './pages/TreinamentoDetalhe';
 import { DataProvider } from './context/DataContext';
 import { AuthProvider } from './context/AuthContext';
 import { ProtectedRoute } from './components/ProtectedRoute';
@@ -29,7 +28,6 @@ function App() {
             <Route path="/intel" element={<Intelligence />} />
             <Route path="/materiais" element={<MateriaisApoio />} />
             <Route path="/treinamentos" element={<TreinamentosAoVivo />} />
-            <Route path="/treinamentos/:id" element={<TreinamentoDetalhe />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Router>
